c701da03b3147b995eeadad3a393e91feae17596
Lakshman Padmanabhan <lakshman.padman@gmail.com>
2020-10-31 22:15:50 -0400

    init commit

--- RandomGuessGame.py
+++ RandomGuessGame.py
@@ -1,10 +1,16 @@
+# importing random
 import random
 
+# generating the random number
 r = random.randint(1,10)
 
+# user's guess/input
 print("Input a number 1-10: ")
 rInp = int(input())
 
+# check if the user's guess is equal to the generated number.
+# it will let the user know that they won.
+# else, then it will let the user know that they lost.
 if r == rInp:
     print("You win!")
     input()
